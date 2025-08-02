41db767676e86c6dd849bfb199a91e45549d1ce7
Mustaq Mujahid Mim <mustaqmujahidmim@gmail.com>
2025-08-02 18:43:46 +0600

    Initial Setup

---
+++ miscellaneous/test/test.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout << "\nC++ is working correctly.\n"
+         << endl;
+    return 0;
+}
---
+++ miscellaneous/test/test.java
@@ -0,0 +1,5 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("\nJava is working correctly.\n");
+    }
+}
