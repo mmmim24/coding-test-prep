39800f2dc18c99d7d67a8be0be7411dfa1a67021
Mustaq Mujahid Mim <mustaq24@student.sust.edu>
2025-08-25 02:44:13 +0600

    cf

---
+++ miscellaneous/codeforces/solution.cpp
@@ -0,0 +1,67 @@
+///|||||||||||||||||||||||||||||||||||///
+///---In the name of Almighty Allah---///
+///|||||||||||||||||||||||||||||||||||///
+#include <bits/stdc++.h>
+#include <unordered_set>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+using vi = vector<int>;
+using pii = pair<int, int>;
+using vpii = vector<pii>;
+using si = set<int>;
+using msi = map<string, int>;
+using minpq = priority_queue<int, vector<int>, greater<int>>;
+#define mmm                           \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(nullptr);                 \
+    cout.tie(nullptr)
+#define pi acos(-1.0)
+#define eps 1e-18
+#define sz (int)1e+9
+#define mod ll(1e8 + 7)
+#define F first
+#define S second
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define ndl "\n"
+#define ws " "
+#define yes "YES\n"
+#define no "NO\n"
+void solve()
+{
+    ll n, ans = 0;
+    cin >> n;
+    vector<ll> g(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> g[i];
+    }
+    sort(g.rbegin(), g.rend());
+    int i;
+    for (i = 0; i < n; i += 2)
+    {
+        ans += g[i];
+    }
+    cout << ans << ndl;
+}
+///|||||||||||||||||||||||||||||||||||///
+///-----"For every problem,there------///
+///------is a solution which is-------///
+///-----simple, fast, and wrong."-----///
+///|||||||||||||||||||||||||||||||||||///
+int main()
+{
+    mmm;
+    int T = 1;
+    cin >> T;
+    // for (int caseno = 1; caseno <= T; caseno++)
+    // while(!cin.eof())
+    // while(true)
+    while (T--)
+    {
+        solve();
+    }
+    return 0;
+}
